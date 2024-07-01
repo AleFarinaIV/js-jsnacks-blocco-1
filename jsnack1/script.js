15c3d6d135c468b05cb2c6cdb6094988f6225f02
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-01 15:02:19 +0200

    [UPD] created FOR structure and added IF statement with response

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,5 +1,12 @@
 const guestList = ["Alessandro Farina", "Lorenzo Pranio", "Gioele Miscia", "Laura Corrado", "Rivaldo Gijon", "Luca Di Santo", "Marianna Coppa"]
 
-const name = prompt("Inserisci qui il tuo nome")
+let name = prompt("Inserisci qui il tuo nome")
 
 let verification = false
+
+for (let i = 0; i < guestList.length; i++) {
+    if (guestList[i].toLowerCase() === name) {
+        verification = true
+        document.getElementById('name-true').innerHTML = "Buonasera, lei fa parte degli invitati"
+    }
+}
