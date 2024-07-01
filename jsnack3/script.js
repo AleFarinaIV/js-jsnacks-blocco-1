a284658304229738b893a3598a76dd5277fab711
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-01 18:37:20 +0200

    [UPD] created first FOR and pushed converted value into void array

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,8 @@
 let userNumber = prompt("Inserisci qui un numero a 4 cifre")
 const strArray = userNumber.split('')
 const intArray = []
+
+for (let i = 0; i < strArray.length; i++) {
+    intArray.push(parseInt(strArray[i]))
+    console.log (intArray)
+}
