485833afc92443a22ddbcf7a67327d36b54994ab
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-01 14:33:52 +0200

    [UPD] used prompt to get user name and created verification var

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,5 @@
 const guestList = ["Alessandro Farina", "Lorenzo Pranio", "Gioele Miscia", "Laura Corrado", "Rivaldo Gijon", "Luca Di Santo", "Marianna Coppa"]
+
+const name = prompt("Inserisci qui il tuo nome")
+
+let verification = false
